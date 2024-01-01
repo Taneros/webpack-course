5a11e6b4a0326966e442ae21559f51456cfd27fa
taneros <taneros@duck.com>
2024-01-01 21:13:09 +0300

    feat: add babel

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -71,11 +71,29 @@ export function buildLoaders(params: { env: IEnvVariable }): ModuleOptions['rule
     exclude: /node_modules/,
   }
 
+  const babelLoader = {
+    test: /\.tsx?$/,
+    exclude: /node_modules/,
+    use: {
+      loader: "babel-loader",
+      options: {
+        presets: [
+          '@babel/preset-env',
+          '@babel/preset-typescript',
+          ['@babel/preset-react', {
+            runtime: isDevMode ? 'automatic' : 'classic',
+          },]
+        ]
+      }
+    }
+  }
+
 
   return [
     svgLoader,
     assetLoader,
     scssLoader,
-    tsLoader,
+    // tsLoader,
+    babelLoader
   ]
 }
