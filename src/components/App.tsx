870488958f52e53c155d773657279d3100d28d33
taneros <taneros@duck.com>
2024-01-02 22:47:36 +0300

    feat: add source map

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -4,8 +4,26 @@ import styles from './App.module.scss'
 import fearImg from '@/assets/fear.png'
 import Cloud from '@/assets/cloud-bolt-svgrepo-com.svg'
 
+// TREE SHAKING
+
+function TODO() {
+  TODO2()
+}
+
+function TODO2() {
+  throw new Error("TODO2");
+  
+}
+
+
+
 export const App = () => {
   const [count, setCount] = useState<number>(0)
+
+  const increment = () => {
+    // setCount(count + 1)
+    TODO()
+  }
 
   if (__PLATFORM__ === 'desktop') {
 
@@ -16,7 +34,7 @@ export const App = () => {
         <Link to={'/shop'}>shop</Link>
         <h1>PLATFORM=desktop</h1>
         <p>{count}</p>
-        <button className={styles.button} onClick={() => setCount(count + 1)}>UP</button>
+        <button className={styles.button} onClick={increment}>UP</button>
         <Cloud fill="green" />
         <Outlet />
       </>
@@ -31,7 +49,7 @@ export const App = () => {
         <Link to={'/shop'}>shop</Link>
         <h1>PLATFORM=mobile</h1>
         <p>{count}</p>
-        <button className={styles.button} onClick={() => setCount(count + 1)}>UP</button>
+        <button className={styles.button} onClick={increment}>UP</button>
         {/* <img src={fearImg} alt="fear" /> */}
         <Cloud fill="green" />
         <Outlet />
